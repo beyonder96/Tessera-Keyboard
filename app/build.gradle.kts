a1e75f8445bc360c6580c2162eee5126d556bd61
beyonder96 <kennedoliveiratm@gmail.com>
2026-07-12 14:17:18 -0300

    chore: bump version code to 2 and update version name to 0.0.1-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.example"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "0.0.1-beta"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
