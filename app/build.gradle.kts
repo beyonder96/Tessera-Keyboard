368a593dc99f9067d8216bc97d8fa2337a5b80fc
beyonder96 <kennedoliveiratm@gmail.com>
2026-09-18 15:01:39 -0300

    chore(release): v0.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.example"
     minSdk = 24
     targetSdk = 36
-    versionCode = 20
-    versionName = "0.0.20"
+    versionCode = 21
+    versionName = "0.0.21"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
